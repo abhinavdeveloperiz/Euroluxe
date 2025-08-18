6a5bc95a36ae7aead69d16e00638ac5308b170d8
abhinavmabhi <dexmarketing192018@gmail.com>
2025-08-18 15:12:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def Products(request):
+    return render(request,'product.html')
+
+from django.views.decorators.cache import cache_page
+
+def About(request):
+    return render(request,'about.html')
---
+++ euroluxe/urls.py
@@ -0,0 +1,17 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('products/',views.Products,name='products'),
+    path('about/',views.About,name='about'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
